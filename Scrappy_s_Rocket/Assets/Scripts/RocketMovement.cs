49b2f4ac8dc389ff117f2791a9170012b48efbec
CyberBlast <ashishdantesparda@gmail.com>
2019-06-29 16:07:55 +0530

    03: Basic Movement done. Needs more tweaking.

---
+++ Scrappy_s_Rocket/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+
+	Rigidbody rigidBody;
+	public float TurnRate = 50.0f;
+	public float Force = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		ProcessInput();
+    }
+
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rigidBody.AddRelativeForce(Vector3.up * Force * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * TurnRate * Time.deltaTime);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(-Vector3.forward * TurnRate * Time.deltaTime);
+		}
+	}
+}
